8872563997c0ea3981b287a8110ecc44e8c5286f
Hohyun.Kim <hohyun1022@naver.com>
2023-10-21 00:53:12 +0900

    fix: boss err, skill err

--- phaser3-project-template/src/utils/sceneManager.js
+++ phaser3-project-template/src/utils/sceneManager.js
@@ -2,3 +2,12 @@ export function loseGame(playingScene) {
   playingScene.m_gameOverSound.play();
   playingScene.scene.start("gameOverScene");
 }
+
+export function winGame(playingScene) {
+  playingScene.m_gameClearSound.play();
+  playingScene.scene.start("gameClearScene", {
+    mobsKilled: playingScene.m_topBar.m_mobsKilled,
+    level: playingScene.m_topBar.m_level,
+    secondElapsed: playingScene.m_secondElapsed,
+  });
+}
